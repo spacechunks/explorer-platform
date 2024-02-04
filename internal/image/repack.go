38c92dabbebf1731ef139463787dfe41ad586e72
Yannic Rieger <riegeryannic@gmail.com>
2024-02-04 02:17:29 +0100

    rename RelPath to StrippedPath and explain this field

--- internal/image/repack.go
+++ internal/image/repack.go
@@ -19,7 +19,7 @@ func Repack(src ociv1.Image, rootPath string, path string) (ociv1.Image, error) 
 	for i := range files {
 		// adjust absolute path so flavor files end up
 		// in the server root directory when appending the layer.
-		files[i].AbsPath = fmt.Sprintf("%s%s", rootPath, files[i].RelPath)
+		files[i].AbsPath = fmt.Sprintf("%s%s", rootPath, files[i].StrippedPath)
 	}
 	log.Printf("append flavor=%s\n", path)
 	img, err := AppendLayerFromFiles(src, files)
