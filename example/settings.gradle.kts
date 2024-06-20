c7d5f80f1a3cfc81d20d626632eaa7242e04ac9a
yannic rieger <riegeryannic@gmail.com>
2024-06-20 15:56:28 +0200

    remove code from previous iteration (#5)
    a lot of things have changed designwise recently and
    most of the code was for experimenting anyway.
    
    now only OCI image processing and network tunnel code
    is left, because these things we will actually need in
    the future.

--- example/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "lbwl-flash"
--- example/src/main/kotlin/cloud/luxor/lbwl/flash/GameData.kt
+++
@@ -1,7 +0,0 @@
-package cloud.luxor.lbwl.flash
-
-import org.bukkit.Location
-
-data class Checkpoint(val location: Location, val timeReached: Long)
-
-data class GameData(val checkpoints: MutableSet<Checkpoint>, val mapSpeed: Int, val spawn: Location)
