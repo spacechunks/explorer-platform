5a9351e789e3be04b232e4247216603649e6025c
Yannic Rieger <riegeryannic@gmail.com>
2024-06-22 20:14:45 +0200

    implement tests for cni

--- internal/tun/cni_test.go
+++ internal/tun/cni_test.go
@@ -21,67 +21,79 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package tun_test
 
 import (
+	"errors"
 	"github.com/containernetworking/cni/pkg/skel"
+	"github.com/spacechunks/platform/internal/mock"
 	"github.com/spacechunks/platform/internal/tun"
-	"github.com/vishvananda/netlink"
-	"github.com/vishvananda/netns"
-	"runtime"
+	"github.com/stretchr/testify/assert"
+	"net"
 	"testing"
+
+	current "github.com/containernetworking/cni/pkg/types/100"
 )
 
-// we use github.com/vishvananda/netns library for testing instead of
-// github.com/containernetworking/plugins/pkg/ns, because it provides
-// us with the ability to create/destroy named network namespaces.
-// the other one does not provide this feature.
-
-func createNetns(t *testing.T) (netns.NsHandle, netns.NsHandle, string) {
-	// lock the OS Thread, so we don't accidentally switch namespaces
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-
-	origin, err := netns.Get()
-	if err != nil {
-		t.Fatalf("create netns: %v", err)
+func TestCNIAdd(t *testing.T) {
+	tests := []struct {
+		name string
+		prep func(*mock.Handler, *skel.CmdArgs)
+		args *skel.CmdArgs
+		err  string
+	}{
+		{
+			name: "everything works fine",
+			args: &skel.CmdArgs{
+				ContainerID: "abc",
+				Netns:       "/path/to/netns",
+				StdinData:   []byte(`{"ipam":{"type": "host-local"}}`),
+			},
+			prep: func(h *mock.Handler, args *skel.CmdArgs) {
+				ips := []*current.IPConfig{
+					{
+						Interface: nil,
+						Address:   net.IPNet{},
+						Gateway:   nil,
+					},
+				}
+				h.EXPECT().
+					AllocIPs("host-local", args.StdinData).
+					Return(ips, nil)
+				h.EXPECT().
+					CreateAndConfigureVethPair(args.ContainerID, args.Netns, ips).
+					Return("veth", nil)
+				h.EXPECT().
+					AttachEgressBPF("veth").
+					Return(nil)
+			},
+		},
+		{
+			name: "dealloc ips on error",
+			args: &skel.CmdArgs{
+				StdinData: []byte(`{"ipam":{"type": "host-local"}}`),
+			},
+			err: "alloc ips: some error",
+			prep: func(h *mock.Handler, args *skel.CmdArgs) {
+				h.EXPECT().
+					AllocIPs("host-local", args.StdinData).
+					Return(nil, errors.New("some error"))
+				h.EXPECT().
+					DeallocIPs("host-local", args.StdinData).
+					Return(nil)
+			},
+		},
 	}
-	handle, err := netns.NewNamed("test")
-	if err != nil {
-		t.Fatalf("create netns: %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var (
+				h = mock.NewHandler(t)
+				c = tun.NewCNI(h)
+			)
+			tt.prep(h, tt.args)
+			err := c.ExecAdd(tt.args)
+			if err != nil && tt.err != "" {
+				assert.EqualError(t, err, tt.err)
+				return
+			}
+			assert.NoError(t, err)
+		})
 	}
-	if err := netns.Set(origin); err != nil {
-		t.Fatalf("set netns: %v", err)
-	}
-	return handle, origin, "test"
-}
-
-func TestAdd(t *testing.T) {
-	created, origin, name := createNetns(t)
-	defer created.Close()
-	defer origin.Close()
-	defer netns.DeleteNamed(name)
-	args := &skel.CmdArgs{
-		ContainerID: "ABC",
-		Netns:       "/var/run/netns/" + name,
-	}
-	if err := tun.CNIFuncs().Add(args); err != nil {
-		t.Fatalf("add: %v", err)
-	}
-	podVeth := getLinkByNS(t, "podABC", created)
-	hostVeth := getLinkByNS(t, "hostABC", origin)
-	if podVeth == nil {
-		t.Fatal("pod veth not found")
-	}
-	if hostVeth == nil {
-		t.Fatal("host veth not found")
-	}
-}
-
-func getLinkByNS(t *testing.T, name string, h netns.NsHandle) netlink.Link {
-	if err := netns.Set(h); err != nil {
-		t.Fatalf("switch netns (%d): %v", int(h), err)
-	}
-	l, err := netlink.LinkByName(name)
-	if err != nil {
-		t.Fatalf("get link by name (%s): %v", name, err)
-	}
-	return l
 }
