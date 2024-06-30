853bc1c46c3952a47ddf93c12c3113ff2bb1c6f4
yannic rieger <yannic.rieger@hetzner-cloud.de>
2024-06-30 11:57:05 +0200

    add first iteration of snat and dnat programs
    currently those are not really production ready.
    map lookups are missing entirely, but the base
    logic is implemented and working. we need to
    figure out how to test these. this will be
    part of a separate MR.

--- internal/tun/bpf/dnat.c
+++ internal/tun/bpf/dnat.c
@@ -23,10 +23,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "vmlinux.h"
 #include "net_helpers.h"
 
-#define IP_SRC_OFF (ETH_HLEN + offsetof(struct iphdr, saddr))
+#define TCP_DPORT_OFF (ETH_HLEN + sizeof(struct iphdr) + offsetof(struct tcphdr, dest))
+#define IP_DST_OFF (ETH_HLEN + offsetof(struct iphdr, daddr))
+#define BPF_F_PSEUDO_HDR 0x10
 
 SEC("tc")
-int snat(struct __sk_buff *ctx)
+int dnat(struct __sk_buff *ctx)
 {
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
@@ -34,17 +36,35 @@ int snat(struct __sk_buff *ctx)
     if (ctx->protocol != bpf_htons(ETH_P_IP))
         return TC_ACT_OK;
 
-    __be32 src = bpf_htonl(0x416c936f);
-    __be32 prev_src;
+    __be16 port;
+    bpf_skb_load_bytes(ctx, TCP_DPORT_OFF, &port, sizeof(__be16));
 
-    bpf_skb_load_bytes(ctx, IP_SRC_OFF, &prev_src, 4);
+    if (bpf_htons(port) != 25565) {
+        return TC_ACT_OK;
+    }
 
-    bpf_skb_store_bytes(ctx, IP_SRC_OFF, &src, sizeof(src), 0);
-    bpf_l3_csum_replace(ctx, IP_CSUM_OFF, prev_src, src, sizeof(src));
-    bpf_l4_csum_replace(ctx, TCP_CSUM_OFF, prev_src, src,  0x10 | sizeof(src));
+    /* d6:35:fc:1e:56:15 */
+    u8 dest_mac[] = {
+        214, 53, 252, 30, 86, 21
+    };
 
-    /* this fills in the l2 address for us */
-    return bpf_redirect_neigh(2, NULL, 0, 0);
+   struct ethhdr *ethh;
+   if (data + sizeof(struct ethhdr) > data_end) {
+        return TC_ACT_OK;
+   }
+
+    ethh = data;
+
+    __builtin_memcpy(ethh->h_dest, dest_mac, ETH_ALEN);
+    __be32 dst = bpf_htonl(0xa000001);
+    __u32 prev_dst;
+
+    bpf_skb_load_bytes(ctx, IP_DST_OFF, &prev_dst, 4);
+    bpf_skb_store_bytes(ctx, IP_DST_OFF, &new_ip, sizeof(dst), 0);
+    bpf_l3_csum_replace(ctx, IP_CSUM_OFF, prev_dst, dst, sizeof(dst));
+    bpf_l4_csum_replace(ctx, TCP_CSUM_OFF, prev_dst, dst,  BPF_F_PSEUDO_HDR | sizeof(dst));
+
+    return bpf_redirect_peer(4, 0);
 }
 
 char _license[] SEC("license") = "GPL";
