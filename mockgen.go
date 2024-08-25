e530297403e10e6911da49d4901bdef98db03346
Yannic Rieger <riegeryannic@gmail.com>
2024-08-25 04:18:00 +0200

    rename cni component from tun to ptpnat
    I am doing this, because ptpnat (port
    to pod network address translation)
    better reflects what the CNI is actually
    doing.

--- mockgen.go
+++ mockgen.go
@@ -1,3 +1,3 @@
 package mockgen
 
-//go:generate go run github.com/vektra/mockery/v2 --srcpkg github.com/spacechunks/platform/internal/tun --name Handler --outpkg mock --output internal/mock --structname Handler --filename cni_handler.go --with-expecter=true --disable-version-string
+//go:generate go run github.com/vektra/mockery/v2 --srcpkg github.com/spacechunks/platform/internal/ptpnat --name Handler --outpkg mock --output internal/mock --structname Handler --filename cni_handler.go --with-expecter=true --disable-version-string
