47b72377be416d21f4d3da6907ee7df9fddc0361
yannic rieger <ybr@76k.io>
2024-11-10 18:59:30 +0100

    handle shutdown scenarios

--- cmd/platformd/main.go
+++ cmd/platformd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
+	"github.com/hashicorp/go-multierror"
 	"github.com/peterbourgon/ff/v3"
 	"github.com/spacechunks/platform/internal/platformd"
 	"log/slog"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 )
 
 func main() {
@@ -14,6 +20,7 @@ func main() {
 		fs     = flag.NewFlagSet("platformd", flag.ContinueOnError)
 
 		proxyServiceListenSock = fs.String("proxy-service-listen-sock", "/var/run/platformd/platformd.sock", "path to the unix domain socket to listen on")
+		criListenSock          = fs.String("cri-listen-sock", "/var/run/crio/crio.sock", "path to the unix domain socket the CRI is listening on")
 		_                      = fs.String("config", "/etc/platformd/config.json", "path to the config file")
 	)
 	if err := ff.Parse(fs, os.Args[1:],
@@ -21,22 +28,43 @@ func main() {
 		ff.WithConfigFileFlag("config"),
 		ff.WithConfigFileParser(ff.JSONParser),
 	); err != nil {
-		die("failed to parse config", err, logger)
+		die(logger, "failed to parse config", err)
 	}
 
 	var (
 		cfg = platformd.Config{
 			ProxyServiceListenSock: *proxyServiceListenSock,
+			CRIListenSock:          *criListenSock,
 		}
+		ctx    = context.Background()
 		server = platformd.NewServer(logger)
 	)
 
-	if err := server.Run(cfg); err != nil {
-		die("failed to start server", err, logger)
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		s := <-c
+		logger.Info("received shutdown signal", "signal", s)
+		cancel()
+	}()
+
+	if err := server.Run(ctx, cfg); err != nil {
+		var multi *multierror.Error
+		if errors.As(err, &multi) {
+			errs := make([]string, 0, len(multi.WrappedErrors()))
+			for _, err := range multi.WrappedErrors() {
+				errs = append(errs, err.Error())
+			}
+			die(logger, "failed to run server", errors.New(strings.Join(errs, ",")))
+			return
+		}
+		die(logger, "failed to run server", err)
 	}
 }
 
-func die(msg string, err error, logger *slog.Logger) {
+func die(logger *slog.Logger, msg string, err error) {
 	logger.Error(msg, "err", err)
 	os.Exit(1)
 }
--- internal/platformd/server.go
+++ internal/platformd/server.go
@@ -1,10 +1,15 @@
 package platformd
 
 import (
+	"context"
 	"fmt"
+	"github.com/envoyproxy/go-control-plane/pkg/cache/v3"
+	"github.com/hashicorp/go-multierror"
 	proxyv1alpha1 "github.com/spacechunks/platform/api/platformd/proxy/v1alpha1"
 	"github.com/spacechunks/platform/internal/platformd/proxy"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	runtimev1 "k8s.io/cri-api/pkg/apis/runtime/v1"
 	"log/slog"
 	"net"
 )
@@ -14,25 +19,52 @@ type Server struct {
 }
 
 func NewServer(logger *slog.Logger) *Server {
-	return &Server{}
+	return &Server{
+		logger: logger,
+	}
 }
 
-func (s *Server) Run(cfg Config) error {
-	unix, err := net.Listen("unix", cfg.ProxyServiceListenSock)
+func (s *Server) Run(ctx context.Context, cfg Config) error {
+	unixSock, err := net.Listen("unix", cfg.ProxyServiceListenSock)
 	if err != nil {
 		return fmt.Errorf("failed to listen on unix socket: %v", err)
 	}
 
+	criConn, err := grpc.NewClient("unix://"+cfg.CRIListenSock, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("failed to create cri grpc client: %w", err)
+	}
+
 	var (
-		mgmtServer  = grpc.NewServer()
+		mgmtServer  = grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
 		proxyServer = proxy.NewServer()
+		xdsCfg      = cache.NewSnapshotCache(true, cache.IDHash{}, nil)
+		criClient   = runtimev1.NewRuntimeServiceClient(criConn)
 	)
 
 	proxyv1alpha1.RegisterProxyServiceServer(mgmtServer, proxyServer)
+	proxy.CreateAndRegisterXDSServer(ctx, mgmtServer, xdsCfg)
 
-	if err := mgmtServer.Serve(unix); err != nil {
-		return fmt.Errorf("failed to serve mgmt server: %w", err)
-	}
+	ctx, cancel := context.WithCancel(ctx)
 
-	return nil
+	var g multierror.Group
+	g.Go(func() error {
+		if err := mgmtServer.Serve(unixSock); err != nil {
+			cancel()
+			return fmt.Errorf("failed to serve mgmt server: %w", err)
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+
+	mgmtServer.GracefulStop()
+	g.Go(func() error {
+		if err := criConn.Close(); err != nil {
+			return fmt.Errorf("cri conn close: %w", err)
+		}
+		return nil
+	})
+
+	return g.Wait().ErrorOrNil()
 }
